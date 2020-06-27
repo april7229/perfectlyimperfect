ba5342f727e5b2d688dc1a2c083b365c26215ed5
april <martinezapril56@yahoo.com>
2020-06-27 16:35:42 -0400

    april

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,128 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './screens/RegisterScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import OrdersScreen from './screens/OrdersScreen';
 
 function App() {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
   const openMenu = () => {
-     document.querySelector(".sidebar").classList.add("open");
-  }
+    document.querySelector('.sidebar').classList.add('open');
+  };
   const closeMenu = () => {
-    document.querySelector(".sidebar").classList.remove("open");
-  }
+    document.querySelector('.sidebar').classList.remove('open');
+  };
   return (
-    <div classNameName="App">
+    <BrowserRouter>
       <div className="grid-container">
-    <header className="header">
-      <div className="brand">
-        <button onClick={openMenu}>
-          &#9776;
-        </button>
-        <a href="index.html">Perfectly Imperfect</a>
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">amazona</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            {userInfo ? (
+              <Link to="/profile">{userInfo.name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#">Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul className="categories">
+            <li>
+              <Link to="/category/Pants">Pants</Link>
+            </li>
+
+            <li>
+              <Link to="/category/Shirts">Shirts</Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/orders" component={OrdersScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+            <Route path="/order/:id" component={OrderScreen} />
+            <Route path="/products" component={ProductsScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/category/:id" component={HomeScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">All right reserved.</footer>
       </div>
-      <div className="header-links">
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
-      </div>
-    </header>
-    <aside className="sidebar">
-      <h3>Shopping Categories</h3>
-      <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-      <ul>
-        <li>
-          <a href="index.html">Cups</a>
-        </li>
-
-        <li>
-          <a href="index.html">Shirts</a>
-        </li>
-        <li>
-        <a href="index.html">Jnpxewlery</a>
-       </li>
-       <li>
-        <a href="index.html">Mirrors</a>
-</li>
-
-      </ul>
-    </aside>
-    <main className="main">
-      <div className="content">
-        <ul className="products">
-          <li>
-            <div className="product">
-              <img className="product-image" src="images/pic1.jpg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">Faith Hope Love</a>
-              </div>
-              <div className="product-brand"></div>
-              <div className="product-price"Aprils creations>$15</div>
-              <div className="product-rating">4.5 Stars (10 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img className="product-image" src="./images/pic2.jpg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">gone with the wind</a>
-              </div>
-              <div className="product-brand">aprilcreations</div>
-              <div className="product-price">$15</div>
-              <div className="product-rating">4.5 Stars (10 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img className="product-image" src="./images/pic4.jpg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">Science</a>
-              </div>
-              <div className="product-brand">april creations </div>
-              <div className="product-price">$15</div>
-              <div className="product-rating">4.5 Stars (10 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img className="product-image" src="./images/pic5.jpg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">cowboy skull</a>
-              </div>
-              <div className="product-brand">aprilcreations</div>
-              <div className="product-price">$15</div>
-              <div className="product-rating">4.5 Stars (10 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img className="product-image" src="./images/pic6.jpg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">nightmare before christmas</a>
-              </div>
-              <div className="product-brand">april creations</div>
-              <div className="product-price">$10</div>
-              <div className="product-rating">4.5 Stars (10 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img className="product-image" src="./images/pic8.jpg" alt="product" />
-              <div className="product-name">
-                <a href="product.html">girl</a>
-              </div>
-              <div className="product-brand">aprilcreations</div>
-              <div className="product-price">$15</div>
-              <div className="product-rating">4.5 Stars (10 Reviews)</div>
-            </div>
-          </li>
-
-        </ul>
-      </div>
-
-    </main>
-    <footer className="footer">
-      All right reserved.
-    </footer>
-  </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
